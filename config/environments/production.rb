1975b976fdc395e70a3a6e1bef9c0d8cc429701f
Daniel Moros <danielousky@gmail.com>
2025-04-24 14:47:20 -0400

    Refactor email configuration in production environment to use environment variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,13 +13,13 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:                'smtp.mailersend.net',
-    port:                   587,
-    domain:                 ENV['PROVIDER_DOMAIN_ADRESS'],
-    user_name:              ENV['USERNAME'],
-    password:               ENV['PASSWORD'],
-    authentication:         'plain',
-    enable_starttls_auto:   true
+    address:            ENV['PROVIDER_EMAIL_SERVER'],
+    port:               ENV['PROVIDER_EMAIL_PORT'],
+    domain:             ENV['PROVIDER_EMAIL_ADDRESS'],
+    user_name:          ENV['PROVIDER_EMAIL_USERNAME'],
+    password:           ENV['PROVIDER_EMAIL_PASSWORD'],
+    authentication:     'plain',
+    enable_starttls_auto: true
   }
 
   config.cache_classes = true
