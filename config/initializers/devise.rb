1cc6eefcae44334820f6d5ee91e260448b9b4bbb
Daniel Moros <danielousky@gmail.com>
2026-02-24 11:11:10 -0400

    Activada notificación por cambio de correo y contraseña en Devise; actualizado mensaje de bienvenida con recomendaciones de seguridad.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -129,10 +129,10 @@ Devise.setup do |config|
   # config.pepper = '7db862f1876d53a98fe72264eb40f77b917644a66206fe246915b85b7faf5525f2ba3e3533b58358f0f05591a8169ba19c27733ac3eb516584e0afe0106b87c7'
 
   # Send a notification to the original email when the user's email is changed.
-  config.send_email_changed_notification = false
+  config.send_email_changed_notification = true
 
   # Send a notification email when the user's password is changed.
-  config.send_password_change_notification = false
+  config.send_password_change_notification = true
 
   # ==> Configuration for :confirmable
   # A period that the user is allowed to access the website even without
